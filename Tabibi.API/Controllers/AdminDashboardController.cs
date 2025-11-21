f36a025c521b03cff7ca6c0a2b4b55ee758089c4
Mohamed Walid <mohamedwalidtharwat2020@gmail.com>
2025-11-21 15:22:28 +0200

    Add GET: admin/dashboard/summary GetSummary endpoint

---
+++ Tabibi.API/Controllers/AdminDashboardController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tabibi.API.Common;
+using Tabibi.API.Database;
+using Tabibi.API.DTOs.AdminDashboard;
+using Tabibi.API.Entities;
+using Tabibi.API.Entities.Enums;
+
+namespace Tabibi.API.Controllers
+{
+    [Authorize(Roles = Roles.Admin)]
+    [Route("admin/dashboard")]
+    [ApiController]
+    public sealed class AdminDashboardController(AppDbContext dbContext) : ControllerBase
+    {
+        [HttpGet("summary")]
+        [ProducesResponseType<AdminDashboardSummaryDto>(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetSummary()
+        {
+            int totalDoctors = await dbContext.Users.OfType<Doctor>().CountAsync();
+
+            int totalPendingDoctors = await dbContext.Users.OfType<Doctor>()
+                .CountAsync(d => d.Status == DoctorStatus.Pending);
+
+            int totalApprovedDoctors = await dbContext.Users.OfType<Doctor>()
+                .CountAsync(d => d.Status == DoctorStatus.Approved);
+
+            int totalPatients = await dbContext.Users.OfType<Patient>().CountAsync();
+
+            AdminDashboardSummaryDto summaryDto = new()
+            {
+                TotalDoctors = totalDoctors,
+                TotalPendingDoctors = totalPendingDoctors,
+                TotalApprovedDoctors = totalApprovedDoctors,
+                TotalPatients = totalPatients
+            };
+
+            return Ok(summaryDto);
+        }
+    }
+}
---
+++ Tabibi.API/DTOs/AdminDashboard/AdminDashboardSummaryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Tabibi.API.DTOs.AdminDashboard
+{
+    public sealed record AdminDashboardSummaryDto
+    {
+        public int TotalDoctors { get; init; }
+        public int TotalPendingDoctors { get; init; }
+        public int TotalApprovedDoctors { get; init; }
+        public int TotalPatients { get; init; }
+    }
+}
